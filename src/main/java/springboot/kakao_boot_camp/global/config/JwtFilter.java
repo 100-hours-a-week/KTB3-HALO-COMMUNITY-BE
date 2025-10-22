5a3f33b21a783d3cb335aa10d73031eddff20bdd
halo <dongjin778@naver.com>
2025-10-22 11:33:52 +0900

    feat : JwtFilter 구현중 ..

---
+++ src/main/java/springboot/kakao_boot_camp/global/config/JwtFilter.java
@@ -0,0 +1,34 @@
+package springboot.kakao_boot_camp.global.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.patterns.IToken;
+import org.hibernate.annotations.Filter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private static final String BEARER = "BEARER ";
+
+    Principal
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(SecurityContextHolder.getContext().getAuthentication() != null){
+
+        }
+    }
+
+
+
+}
