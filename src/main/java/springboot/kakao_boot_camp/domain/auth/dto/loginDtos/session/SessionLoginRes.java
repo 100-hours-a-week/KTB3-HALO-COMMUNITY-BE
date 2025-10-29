0fa875a8935b73940ea332d2abee09de07c23550
halo <dongjin778@naver.com>
2025-10-29 16:49:03 +0900

    feat : 사용자 로그인 시, User Id만 발급받게 설정

--- src/main/java/springboot/kakao_boot_camp/domain/auth/dto/loginDtos/session/SessionLoginRes.java
+++ src/main/java/springboot/kakao_boot_camp/domain/auth/dto/loginDtos/session/SessionLoginRes.java
@@ -3,15 +3,11 @@ package springboot.kakao_boot_camp.domain.auth.dto.loginDtos.session;
 import springboot.kakao_boot_camp.domain.user.entity.User;
 
 public record SessionLoginRes(
-        Long userId,
-        String email,
-        String nickName
+        Long userId
 ) {
     public static SessionLoginRes from(User user) {
         return new SessionLoginRes(
-                user.getId(),
-                user.getEmail(),
-                user.getNickName()
+                user.getId()
         );
     }
 }
