09b9f77c3586e4814f110b2319524929ff38d237
halo <dongjin778@naver.com>
2025-11-02 22:40:35 +0900

    chore : 인증 필터 Spring Security 의존성 제거

--- src/main/java/springboot/kakao_boot_camp/security/config/WebConfig.java
+++ src/main/java/springboot/kakao_boot_camp/security/config/WebConfig.java
@@ -12,11 +12,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springboot.kakao_boot_camp.domain.auth.util.CustomPasswordEncoder;
 import springboot.kakao_boot_camp.domain.auth.util.JwtUtil;
 import springboot.kakao_boot_camp.security.CustomSecurity.filter.CustomJwtFilter;
-import springboot.kakao_boot_camp.security.CustomSecurity.filter.CustomSessionFilter;
 
 @Configuration
 @Profile("custom-security")
@@ -24,30 +21,8 @@ import springboot.kakao_boot_camp.security.CustomSecurity.filter.CustomSessionFi
 public class WebConfig {
     private final JwtUtil jwtUtil;
 
-    @Value("${auth.type}")  // ← yml에 auth.type 있어야 함
-    private String authType;
-
 
     @Bean
-    public FilterRegistrationBean<?> authFilter() {
-        if ("jwt".equalsIgnoreCase(authType)) {
-            return createJwtFilter();
-        } else if ("session".equalsIgnoreCase(authType)) {
-            return createSessionFilter();
-        }
-        throw new IllegalArgumentException("존재하지 않는 필터입니다. (yml-auth-type 확인 필요)");  // ← 둘 다 아닐 때만 실행
-    }
-
-    public FilterRegistrationBean<CustomSessionFilter> createSessionFilter() {
-        CustomSessionFilter customSessionFilter = new CustomSessionFilter();
-        FilterRegistrationBean<CustomSessionFilter> bean = new FilterRegistrationBean<>();
-        bean.setFilter(customSessionFilter);
-        bean.addUrlPatterns("/api/*");
-        bean.setOrder(1);
-        return bean;
-    }
-
-
     public FilterRegistrationBean<CustomJwtFilter> createJwtFilter() {
         CustomJwtFilter customJwtFilter = new CustomJwtFilter(jwtUtil);
         FilterRegistrationBean<CustomJwtFilter> bean = new FilterRegistrationBean<>();
